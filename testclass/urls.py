f58a7acba0c207d048819377d4c7810ebebb6590
Kazuya Yuasa <kazuya.yuasa@careerbeat.jp>
2018-10-29 00:23:04 +0900

    FactoryMethodパターンを作ってみる。

--- testclass/urls.py
+++ testclass/urls.py
@@ -3,5 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TestIndexView.as_view(), name='index')
+    path('', views.TestIndexView.as_view(), name='index'),
+    path(
+        'factory_method/',
+        views.FactoryMethodSample.as_view(),
+        name='factory_method'
+    ),
+    path(
+        'abstract_factory/',
+        views.AbstractFactorySample.as_view(),
+        name='abstract_factory'
+    ),
 ]
